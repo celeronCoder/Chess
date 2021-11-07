f2f1f783bd203559437bfb6a12f500556bd1dfbd
Bhardwaj <khushalbhardwaj0111@gmail.com>
2021-11-07 14:08:49 +0000

    feat: added new factory method create Tile
    - made the tile Construtor private so that can only be accessed from the tile class and it's subclasses. - created an  map to generate all possible empty tile. - created a new factory method classed  that return an empty tile of the given tileCoordinate when no piece on it and an occupied one when given a Piece.

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,14 +1,34 @@
 package com.chess.engine.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
 
 public abstract class Tile {
     // protected: can only be accessed by itself and it's subclasses
     // final: to makes it a constant-Immutable 
     protected final int tileCoordinate;
 
-    Tile (int tileCoordinate) {
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private Tile (int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
+    }
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for (int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
     }
 
     public abstract boolean isTileOccupied();

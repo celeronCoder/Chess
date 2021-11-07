9779f39f092fb7eade9a1b654a443dd3af42bec9
Bhardwaj <khushalbhardwaj0111@gmail.com>
2021-11-07 14:48:46 +0000

    feat: base piece class

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,5 +1,13 @@
 package com.chess.engine.pieces;
 
+import com.chess.engine.Alliance;
+
 public class Piece {
-    
+    protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+
+    Piece(final int piecePosition, final Alliance pieceAlliance) {
+        this.piecePosition = piecePosition;
+        this.pieceAlliance = pieceAlliance;
+    }
 }

6d18165f27469e2bf10c00b8b0780bae7ac33e34
Bhardwaj <khushalbhardwaj0111@gmail.com>
2021-11-07 15:00:45 +0000

    feat: fixes #13

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,8 +1,12 @@
 package com.chess.engine.pieces;
 
-import com.chess.engine.Alliance;
+import java.util.List;
 
-public class Piece {
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+public abstract class Piece {
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
 
@@ -10,4 +14,6 @@ public class Piece {
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
     }
+
+    public abstract List<Move> calculateLegalMoves(final Board board);
 }

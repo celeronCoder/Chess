31ae3e890b382791f3ed50c41e30e12d227ab472
Bhardwaj <khushalbhardwaj0111@gmail.com>
2021-11-08 14:57:00 +0000

    feat: added base Rook class

---
+++ src/com/chess/engine/pieces/Rook.java
@@ -0,0 +1,70 @@
+package com.chess.engine.pieces;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.chess.engine.board.Move.AttackMove;
+import com.chess.engine.board.Move.MajorMove;
+
+public class Rook extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_COORDINATES = { -8, -1, 1, 8 };
+
+
+    Rook(final int piecePosition, final Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(final Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int currentCandidateOffset: CANDIDATE_MOVE_COORDINATES) {
+            int candidateDestinationCoordinate = this.piecePosition;
+
+            while (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                if (isFirstColumnExclusion(currentCandidateOffset, candidateDestinationCoordinate) ||
+                    isEighthColumnExclusion(currentCandidateOffset, candidateDestinationCoordinate)) {
+                    break;
+                }
+                
+                candidateDestinationCoordinate += currentCandidateOffset;
+                
+                if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                
+                    if (!candidateDestinationTile.isTileOccupied()) {
+                        // non-attacking move
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    } else {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+                        if (this.pieceAlliance != pieceAlliance) {
+                            // enemy piece - attacking move
+                            legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return Collections.unmodifiableList(legalMoves);
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentCandidate, final int candidateDestinationCoordinate) {
+        return BoardUtils.FIRST_COLUMN[candidateDestinationCoordinate] && (currentCandidate == -1);
+    }
+
+    private static boolean isEighthColumnExclusion(final int currentCandidate, final int candidateDestinationCoordinate) {
+        return BoardUtils.EIGHTH_COLUMN[candidateDestinationCoordinate] && (currentCandidate == 1);
+    }
+    
+}
